067c5d18811a570eebf6a2122f02730944f87262
David Mareš <the.dvd258@gmail.com>
2022-11-22 15:18:20 +0100

    Initnial commit

---
+++ MultipleScoreCounter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MultipleScoreCounter.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ MultipleScoreCounter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace MultipleScoreCounter.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
